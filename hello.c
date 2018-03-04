03f61f272b078b0a524b51f640e27702b931aa05
shubhuiitk <shubhuiitk@gmail.com>
2018-03-05 03:16:04 +0530

    new branch

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 void main(){
 	  printf("Helloworld!\n");
 	  printf("This must be a monolithic design\n");
@@ -9,3 +10,6 @@ void main(){
 void microkernel_sendmsg(char *a){
 	 printf("microkernel: %s\n", a);
 	  }
+void microkernel_getmsg(char *b){
+	 //TODO: getmsg feature
+	 }
